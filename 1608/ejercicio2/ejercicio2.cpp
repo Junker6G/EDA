4d008af03525a9d317da61ca0783cc2a2e317026
Junker6G <80859121+Junker6G@users.noreply.github.com>
2022-08-17 01:03:20 -0300

    sigo sin saberle a las listas

--- 1608/ejercicio2/ejercicio2.cpp
+++ 1608/ejercicio2/ejercicio2.cpp
@@ -15,30 +15,33 @@ typedef nodoLista *ListaInt;
 
 void imprimirlista(ListaInt);
 ListaInt retornarPositivos(int elem, ListaInt L);
+ListaInt retornarPositivos2(int elem, ListaInt L);
 
 int main()
 {
     ListaInt L1 = NULL;
 
-    L1 = retornarPositivos(1, L1);
-    L1 = retornarPositivos(23, L1);
-    L1 = retornarPositivos(-44, L1);
-    L1 = retornarPositivos(22, L1);
-    L1 = retornarPositivos(5, L1);
-    L1 = retornarPositivos(-12, L1);
-    L1 = retornarPositivos(-42, L1);
+    L1 = retornarPositivos2(1, L1);
+    L1 = retornarPositivos2(23, L1);
+    L1 = retornarPositivos2(-44, L1);
+    L1 = retornarPositivos2(22, L1);
+    L1 = retornarPositivos2(5, L1);
+    L1 = retornarPositivos2(-12, L1);
+    L1 = retornarPositivos2(-42, L1);
 
-	imprimirlista(L1);
-    }
+    imprimirlista(L1);
+}
 
-ListaInt retornarPositivos(int elem, ListaInt L){
+ListaInt retornarPositivos(int elem, ListaInt L)
+{
     ListaInt nuevo = new nodoLista;
     nuevo->info = elem;
     if (L == NULL || L->info > 0)
     {
         nuevo->sig = L;
         return nuevo;
-    }else
+    }
+    else
     {
         ListaInt aux = L;
         while (aux->sig != NULL && aux->sig->info >= 0)
@@ -49,14 +52,39 @@ ListaInt retornarPositivos(int elem, ListaInt L){
         aux->sig = nuevo;
         return L;
     }
-
 }
 
-void imprimirlista(ListaInt L){
-	cout <<"Impresion de lista"<< endl;
-	while (L != NULL){
-		cout <<" | " << L->info << " | - ";
-		L = L->sig;
-	}
-	cout << "#" << endl;
+ListaInt retornarPositivos2(int elem, ListaInt L)
+{
+    if (L == NULL)
+    {
+        return L;
+    }
+    else
+    {
+        if (L->info > 0)
+        {
+            ListaInt nuevo = new nodoLista;
+            nuevo->info = elem;
+            ListaInt aux = L;
+            aux = L;
+            L = nuevo;
+        }
+        else
+        {
+            retornarPositivos2(elem, L->sig);
+        }
+    }
+    
+}
+
+void imprimirlista(ListaInt L)
+{
+    cout << "Impresion de lista" << endl;
+    while (L != NULL)
+    {
+        cout << " | " << L->info << " | - ";
+        L = L->sig;
+    }
+    cout << "FIN" << endl;
 }
