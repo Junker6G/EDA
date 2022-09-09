cbd009dbb412fd0beb7c2619bffb8dd972ea35ed
Joacox <j.sellanes.p03@gmail.com>
2022-09-09 12:29:45 -0300

    ANACHI

--- 0909/main.cpp
+++ 0909/main.cpp
@@ -1,38 +1,56 @@
 #include <iostream>
 using namespace std;
 
+#define MIN_INT -10000;
 
-struct nodoABChar {
+struct nodoABChar
+{
     char info;
-    nodoABChar * izq;
-    nodoABChar * der;
+    nodoABChar *izq;
+    nodoABChar *der;
 };
 
-typedef nodoABChar * ABChar;
+typedef nodoABChar *ABChar;
 
-struct nodoListaChar {
-    char info;
-    nodoListaChar * sig;
+struct nodoAB
+{
+    int info;
+    nodoAB *izq;
+    nodoAB *der;
 };
 
-typedef nodoListaChar * ListaChar;
+typedef nodoAB *ABInt;
 
-ListaChar cons (ListaChar L, char e);
-ListaChar merge (ListaChar L1, ListaChar L2);
+struct nodoListaChar
+{
+    char info;
+    nodoListaChar *sig;
+};
+
+typedef nodoListaChar *ListaChar;
+
+// PROTOTIPO
+ListaChar cons(ListaChar L, char e);
+ListaChar merge(ListaChar L1, ListaChar L2);
+ABInt multiplicador(ABInt AB, int i);
+void imprimir_arbol (ABChar a);
+ABChar construir_arbol (ABInt subarbol_izq, int raiz, ABInt subarbol_der);
 
 int main()
 {
     return 0;
 }
 
-ListaChar cons (ListaChar L, char e) {
+ListaChar cons(ListaChar L, char e)
+{
     ListaChar nuevo = new nodoListaChar;
     nuevo->info = e;
     nuevo->sig = L;
     return nuevo;
 }
 
-ListaChar merge (ListaChar L1, ListaChar L2) {
+ListaChar merge(ListaChar L1, ListaChar L2)
+{
     ListaChar aux = L1;
     while (aux != NULL && aux->sig)
         aux = aux->sig;
@@ -40,4 +58,47 @@ ListaChar merge (ListaChar L1, ListaChar L2) {
         return L2;
     aux->sig = L2;
     return L1;
+}
+
+ABChar construir_arbol (ABChar subarbol_izq, char raiz, ABChar subarbol_der) {
+    ABChar a = new nodoABChar;
+    a->info = raiz;
+    a->izq = subarbol_izq;
+    a->der = subarbol_der;
+    return a;
+}
+
+
+void imprimir_arbol (ABChar a) {
+    if (a != NULL) {
+        imprimir_arbol (a->izq);
+        cout << "|" << a->info << "|  ";
+        imprimir_arbol(a->der); 
+    }
+}
+
+ABInt multiplicador(ABInt AB, int i)
+{
+    if (AB == NULL)
+    {
+        return 0;
+    }
+    AB->info = AB->info * i;
+    multiplicador(AB->izq, i);
+    multiplicador(AB->der, i);
+
+    return AB;
+}
+
+int retornar_nivel(ABInt abb, int i){
+    if (abb == NULL)
+    {
+        return 0;
+    }
+    if (abb->info == i)
+    {
+        return 1;
+    }
+    retornar_nivel(abb->izq, i);
+    retornar_nivel(abb->der, i);
 }
